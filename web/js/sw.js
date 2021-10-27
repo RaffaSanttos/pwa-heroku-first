d6c9d73041f700d00e9043c827a97732b4015646
Sr_Darkpool <atomix.luke@gmail.com>
2021-10-27 12:05:21 -0300

    correção de erro no código referente ao cache

--- web/js/sw.js
+++ web/js/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e =>{
     e.waitUntill(
-        cahes.open('static-v1')
+        caches.open('static-v1')
         .then(cache => {
             cache.add('/marca_4_pontas')
         })
